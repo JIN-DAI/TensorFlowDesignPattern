104851014a673d4db2a1190e14eaf11b20fe0ef7
Shawn Hu <hushuangwei@gmail.com>
2017-07-09 22:27:19 +0800

    Add files via upload

--- RNNwithDecorator.py
+++ RNNwithDecorator.py
@@ -1,47 +1,11 @@
 # Working example for my blog post at:
 # https://danijar.github.io/structuring-your-tensorflow-models
-import functools
+
 import tensorflow as tf
 from tensorflow.contrib import rnn
 from tensorflow.examples.tutorials.mnist import input_data
 
-
-def doublewrap(function):
-    """
-    A decorator decorator, allowing to use the decorator to be used without
-    parentheses if not arguments are provided. All arguments must be optional.
-    """
-    @functools.wraps(function)
-    def decorator(*args, **kwargs):
-        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
-            return function(args[0])
-        else:
-            return lambda wrapee: function(wrapee, *args, **kwargs)
-    return decorator
-
-
-@doublewrap
-def define_scope(function, scope=None, *args, **kwargs):
-    """
-    A decorator for functions that define TensorFlow operations. The wrapped
-    function will only be executed once. Subsequent calls to it will directly
-    return the result so that operations are added to the graph only once.
-    The operations added by the function live within a tf.variable_scope(). If
-    this decorator is used with arguments, they will be forwarded to the
-    variable scope. The scope name defaults to the name of the wrapped
-    function.
-    """
-    attribute = '_cache_' + function.__name__
-    name = scope or function.__name__
-    @property
-    @functools.wraps(function)
-    def decorator(self):
-        if not hasattr(self, attribute):
-            with tf.variable_scope(name, *args, **kwargs):
-                setattr(self, attribute, function(self))
-        return getattr(self, attribute)
-    return decorator
-
+from Utils import doublewrap, define_scope
 
 class Model:
 
@@ -90,7 +54,7 @@ class Model:
     def optimize(self):
         cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.prediction,
                                                                       labels=self.label))
-        optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+        optimizer = tf.train.AdamOptimizer(0.01).minimize(cost)
         return optimizer
 
 
@@ -128,11 +92,11 @@ def main():
 
     for _ in range(10):
       texts, labels = mnist.test.images, mnist.test.labels
-      texts = texts.reshape((batch_size, 28, 28))
+      texts = texts.reshape((-1, 28, 28))
       error = sess.run(model.error, {text: texts, label: labels})
       print('Test error {:6.2f}%'.format(100 * error))
-      for _ in range(10000):
-        texts, labels = mnist.train.next_batch(100)
+      for _ in range(1000):
+        texts, labels = mnist.train.next_batch(batch_size)
         texts = texts.reshape((batch_size, 28, 28))
         sess.run(model.optimize, {text: texts, label: labels})
 
